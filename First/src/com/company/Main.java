049e9c098acf320c020e5e4f3adf6abfee0c5c72
Sobo_uo43 <Sobo_uo43@student.itstep.org>
2024-09-04 16:46:34 +0300

    HW9

--- First/src/com/company/Main.java
+++ First/src/com/company/Main.java
@@ -5,35 +5,43 @@ import java.util.Scanner;
 import java.util.Random;
 
 /** Java-DOC comment */
+
+class Demo {
+    public void show() {
+        System.out.println("Метод суперкласса");
+    }
+}
+
+interface Hello {
+    void show();
+}
+
 public class Main {
-    public static void main(String[] args) {
-        /*Scanner input = new Scanner(System.in);  // для чтения ввода с консоли, System.in - стандартный поток ввода (обычно клавиатура)
-
-        System.out.print("Input num1: ");
-        float num1 = input.nextFloat();
-
-        System.out.print("Input num2: ");
-        float num2 = input.nextFloat();
-
-        // когда nextLine() вызывается сразу после nextFloat(), он считывает оставшийся символ новой строки, а не ожидает ввода от пользователя
-        input.nextLine();
-
-        System.out.print("Input action: ");
-        String action = input.nextLine();
-
-        float res = switch (action) {
-            case "+" -> num1 + num2;
-            case "-" -> num1 - num2;
-            case "*" -> num1 * num2;
-            case "/" -> num2 != 0 ? num1 / num2 : -1;
-            default -> -1;
-        };
-        if (res != -1) {
-            System.out.println("The result is: " + res);
+    static Hello hello = new Hello() {
+        public void show() {
+            System.out.println("Метод внутреннего анонимного класса");
         }
-        else {
-            System.out.println("Invalid input...");
-        }*/
-        
+    };
+
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.show();
+
+        Demo demo2 = new Demo() {
+            @Override
+            public void show() {
+                System.out.println("Метод внутреннего анонимного класса");
+            }
+        };
+        demo2.show();
+
+        // ----------------------------------
+
+        System.out.println("-".repeat(30));
+        hello.show();
+
+        // ----------------------------------
+
+        System.out.println("-".repeat(30));
     }
 }
